ee2be71065c3bcf1c0fcf8d1c4dc51cbbac6a382
Dikshant098 <103027816+Dikshant098@users.noreply.github.com>
2023-10-19 16:22:01 +0530

    sidebar done

--- src/components/dashboard/sidebar/Sidebar.jsx
+++ src/components/dashboard/sidebar/Sidebar.jsx
@@ -2,12 +2,38 @@ import React from 'react'
 import { AiOutlineDashboard } from 'react-icons/ai'
 import { BsHouse, BsTable, BsGrid, BsPeople, BsPersonCircle } from 'react-icons/bs'
 import '../sidebar/sidebar.scss'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function Sidebar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    // const [toggleProfile, settoggleProfile] = useState(false)
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+
+    useEffect(() => {
+        const profile = document.querySelector('#Profile');
+        if (profile) {
+            profile.addEventListener('click', toggleSidebar);
+        }
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            if (profile) {
+                profile.removeEventListener('click', toggleSidebar);
+            }
+        };
+    }, [isSidebarOpen]);
+
+    const sidebarTransform = isSidebarOpen ? 'translateX(0%)' : 'translateX(110%)';
+
     return (
-        <div className='sidebar container'>
+        <div className='sidebar' style={{ transform: sidebarTransform }}>
             <div className="row">
-                <div className='bg-dark col-auto min-vh-100 d-flex justify-content-between flex-column' style={{width:'100%'}}>
+                <div className='bg-dark col-auto min-vh-100 d-flex justify-content-between flex-column' style={{ width: '100%' }}>
                     <div>
                         <a href="#" className='text-decoration-none text-white d-flex align-items-center ms-3 mt-2  ' aria-current="page">
                             <span className='ms-1 fs-4'>Your Profile</span>
@@ -48,7 +74,7 @@ function Sidebar() {
                     </div>
                     <div className="Yourself dropdown open">
                         <a className="text-decoration-none text-white dropdown-toggle fs-5 p-3 d-flex align-items-center" type="button" id="triggerid" data-bs-toggle="dropdown" aria-expanded="false">
-                        <BsPersonCircle className='fs-5' />
+                            <BsPersonCircle className='fs-5' />
                             <span className='ms-2'>YourSelf</span>
                         </a>
                         <div className="dropdown-menu bg-white" aria-labelledby="triggerid">
--- src/screens/dashboard/DashboardScreen.jsx
+++ src/screens/dashboard/DashboardScreen.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
-import Sidebar from '../../components/dashboard/sidebar/Sidebar'
+// import Footer from '../../components/footer/Footer'
+// import Sidebar from '../../components/dashboard/sidebar/Sidebar'
 
 const DashboardScreen = () => {
     return (
         <React.Fragment>
             
             <Header />
+            <div style={{marginTop:'150px'}}>
             <Outlet />
-            <Sidebar/>
+            </div>
             {/* <Footer /> */}
         </React.Fragment>
     )
