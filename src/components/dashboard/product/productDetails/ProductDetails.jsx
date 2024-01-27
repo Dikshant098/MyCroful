c87f2422fac39c60179ba6c2c9e970c4a78e6ede
Dikshant098 <103027816+Dikshant098@users.noreply.github.com>
2024-01-27 16:37:51 +0530

    Product Details code added

--- src/components/dashboard/product/productDetails/ProductDetails.jsx
+++ src/components/dashboard/product/productDetails/ProductDetails.jsx
@@ -1,86 +1,136 @@
-import React, { useEffect, useState } from 'react'
-import '../productDetails/ProductDetails.scss'
-import { useParams } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { getProductDetails } from '../../../../redux/productDetails/productDetailsAction'
+import React, { useState } from 'react'
+import Madhuri from '../../../../assets/images/Products/Yeola-Meenakari-Kalanjali-Paithani-scaled.jpeg'
+import one from '../../../../assets/images/sarees/saree-1.jpeg';
+import two from '../../../../assets/images/sarees/saree-2.jpeg';
+import three from '../../../../assets/images/sarees/saree-3.jpeg';
+import four from '../../../../assets/images/sarees/saree-4.jpeg';
+import five from '../../../../assets/images/sarees/saree-5.jpeg';
 
 
-const ProductDetails = () => {
-  const dispatch = useDispatch()
-  const productDetailsResponse = useSelector((state) => state.productDetailsReducer);
-  const { loading, success, payload } = productDetailsResponse;
-  const [productDetails, setProductDetails] = useState({})
-  const [imgUrl, setImgUrl] = useState("")
+function ProductDetails() {
 
-  useEffect(() => {
-    if (payload) {
-      setProductDetails(payload);
-      setImgUrl(payload?.images[0].image);
-      console.log(imgUrl);
-      // console.log(productDetails);
+  const firstSetCardData = [
+    { title: 'Card 1', text: 'Some quick example text for Card 1.', img: one },
+    { title: 'Card 2', text: 'Some quick example text for Card 2.', img: two },
+    { title: 'Card 3', text: 'Some quick example text for Card 3.', img: three },
+    { title: 'Card 4', text: 'Some quick example text for Card 4.', img: four },
+    { title: 'Card 5', text: 'Some quick example text for Card 5.', img: five },
+  ];
+
+
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
     }
-
-  }, [payload, imgUrl])
-
-  const params = useParams()
-  // const imgUrl = params.id
-
-  console.log(params.imgUrl);
-
-  // useEffect(() => {
-  //   dispatch(getProductDetails(params.id))
-  // }, [])
-
-  // const imgUrl = "https://www.mystore.in/s/62ea2c599d1398fa16dbae0a/"
+  };
 
   return (
-    // <div className='product_details' >
-    //   <div className='image_div'>
-    //     <img src='https://croful.com/wp-content/uploads/2023/10/Diwali-Special-Earrings1-1.jpeg
-    //       ' alt='Product' style={{ width: '200px', height: '200px' }} className='zoom-on-hover' />
-
-    //   </div>
-    //   <div className='other_div  d-flex justify-content-center d-col'>
-    //     <h3>Special Offer</h3>
-    //     <label htmlFor='color'>Select Color:</label>
-    //     <input type='text' id='color' name='color' />
-    //     <br />
-    //     <label htmlFor='quantity'>Quantity:</label>
-    //     <input type='number' id='quantity' name='quantity' defaultValue='1' />
-    //     <button>+</button>
-    //   </div>
-    // </div>
-
-
-
-    <div className='product_details'>
-      <div className="container ">
-        <div className='row my-5'>
-          <div className='col-lg-4 col-md-6 col-sm-12'>
-            <div className='box'>
-              <img src={"https://www.mystore.in/s/62ea2c599d1398fa16dbae0a/"+imgUrl} className='img-fluid product_img' alt="" />
-            </div>
+    <div>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          {/* Left Section - Image */}
+          <div className="col-md-6">
+            <img
+              src={Madhuri}
+              alt="Placeholder Image"
+              className="img-fluid"
+              style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+            />
           </div>
-          <div className='col-lg-4 col-md-6 col-sm-12'>
-            <div className='box'>
-              <h3 className='text-uppercase'>{productDetails?.name}</h3>
-              <h3 className='text-uppercase price my-3' > {"₹ "+ productDetails?.price}</h3>
-              <h3 className='seller_name mb-4'>{productDetails?.seller_name}</h3>
-              <div className="input-group">
-                <input type="number" className='quantity p-3' value={1} />
-                <button className='text-uppercase btn btn-dark rounded-0 add_to_cart'>Add To cart</button>
+
+          {/* Right Section - Description */}
+          <div className="col-md-6">
+            <div className="card" style={{ border: 'none' }}>
+              <div className="card-body">
+                <h5 className="card-title h4">MADHURI DIXIT SAME COLLECTION YEOLA MEENAKARI KALANJALI PAITHANI</h5>
+                <p className="card-text">
+                  ₹3,400.00
+                </p>
+                <p className="card-text">
+                  Sold By:(Redirect to the shop)
+                </p>
+
+                {/* Counting box */}
+                <div className='d-flex flex-row ' style={{}}>
+                  <div style={{ width: '120px', padding: '10px', border: '1px solid #ccc', borderRadius: '8px', textAlign: 'center' }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                      <button className='d-flex align-items-center justify-content-center' onClick={decreaseCount} style={{ background: 'transparent', height: '20px', width: '20px', border: 'none' }} >-</button>
+                      <span style={{ fontSize: '1.1em', fontWeight: 'bold' }}>{count}</span>
+                      <button className='d-flex align-items-center justify-content-center' onClick={increaseCount} style={{ background: 'transparent', height: '20px', width: '20px', border: 'none' }} >+</button>
+                    </div>
+                  </div>
+                  <button className='btn btn-dark ms-3' style={{ width: '10vw' }} > Add to Cart</button>
+                </div>
+
+                {/* Accordian with three sections */}
+                <div className="accordion accordion-flush mt-5" id="accordionFlushExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header fw-swmibold">
+                      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Product Details
+                      </button>
+                    </h2>
+                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div className="accordion-body">Body 1</div>
+                    </div>
+                  </div>
+                  <div className="accordion-item">
+                    <h2 className="accordion-header fw-swmibold">
+                      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        Product Description
+                      </button>
+                    </h2>
+                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div className="accordion-body">Body 2</div>
+                    </div>
+                  </div>
+                  <div className="accordion-item">
+                    <h2 className="accordion-header fw-swmibold">
+                      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        Vendor Details
+                      </button>
+                    </h2>
+                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div className="accordion-body">Body 3</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className='col-lg-4 col-md-6 col-sm-12'>
-            <div className='box'>
-              <h3 class="widget-title" >CART</h3>
-              <h3 class="widget-title">Recently Viewed Products</h3>
-            </div>
+        </div>
+
+        {/* Related Products */}
+        <div className='mt-5'>
+          <div className='h3 d-flex justify-content-center'>Other Products from Sellers</div>
+          <div className='d-flex justify-content-around mt-5'>
+            {firstSetCardData.map((card, index) => (
+              <div key={index} className='card' style={{
+                width: '180px', boxShadow: '2px 0px 10px rgba(210, 208, 255, 1)', minHeight: '350px',
+                position: 'relative'
+              }}>
+                <img src={card.img} className='card-img-top' alt={`Card ${index + 1}`} />
+                <div className='card-body' style={{ height: '120px' }}>
+                  <h5 className='card-title'>{card.title}</h5>
+                  <p className='card-text'>{card.text}</p>
+                  <a href='#' className='btn fw-semibold' style={{ border: '2px solid black', background: 'linear-gradient(to bottom, rgba(152, 210, 236, 1), rgba(0, 0, 0, 0))' }}>
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
+
+        {/* Description Of Product */}
+
       </div>
-    </div >
+    </div>
   )
 }
 
