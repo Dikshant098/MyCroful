dfaff116ee86301859011619bd389b7599e30688
Harshu0132 <harshalmaheshram10@gmail.com>
2024-03-13 11:04:18 +0530

    profile api integrated

--- src/components/dashboard/profile/Profile.jsx
+++ src/components/dashboard/profile/Profile.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineModeEdit } from 'react-icons/md';
 import '../profile/Profile.scss';
+import axios from 'axios';
+import { BASE_URL } from '../../../constants/baseUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Profile = () => {
     const [showPopup, setShowPopup] = useState(false);
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
+    const [userData, setUserData] = useState();
 
     const togglePopup = () => {
         setShowPopup(!showPopup);
@@ -14,12 +19,67 @@ const Profile = () => {
     const handleSubmit = () => {
         // Handle form submission
         console.log('Submitted:', firstName, lastName);
-        // Close the popup
-        setShowPopup(false);
+        if (firstName == '') {
+            toast.warning("firstName required !!", {
+                autoClose: 1000,
+            })
+            return
+        } else if (lastName == '') {
+            toast.warning("lastName required !!", {
+                autoClose: 1000,
+            })
+            return
+        } else {
+
+            // Close the popup
+            let obj = {
+                firstName, lastName
+            }
+            updateUserData(obj)
+            setShowPopup(false);
+        }
+
     };
+
+    const updateUserData = async (obj) => {
+        const _id = localStorage.getItem('Croful')
+        const url = BASE_URL + 'user/updateUserById/' + _id
+        try {
+            const { data } = await axios.put(url, obj)
+            const userId = localStorage.getItem('Croful')
+            getUserDataById(userId)
+            toast.success("Profile successfully updated !!", {
+                autoClose: 1000,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+    useEffect(() => {
+        const userId = localStorage.getItem('Croful')
+        // console.log(userId);
+        getUserDataById(userId)
+
+    }, [])
+
+    const getUserDataById = async (_id) => {
+        const url = BASE_URL + 'user/findUserById/' + _id
+        try {
+            const { data } = await axios.get(url);
+            setUserData(...data)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return (
         <div className="container">
+            <ToastContainer />
             <div
                 className="container-fluid pb-5"
                 style={{
@@ -54,19 +114,19 @@ const Profile = () => {
                         <div className="col">
                             <div className="row">
                                 <span>First Name</span>
-                                <span>Update details</span>
+                                <span>{userData?.firstName}</span>
                             </div>
                         </div>
                         <div className="col">
                             <div className="row">
                                 <span>Last Name</span>
-                                <span>Update details</span>
+                                <span>{userData?.lastName}</span>
                             </div>
                         </div>
                         <div className="col">
                             <div className="row">
                                 <span>Phone</span>
-                                <span>Update details</span>
+                                <span>{userData?.mobile}</span>
                             </div>
                         </div>
                     </div>
